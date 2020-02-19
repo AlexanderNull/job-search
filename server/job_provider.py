df4c369c8bb4e8e71b237cdb8b285c85d052951c
Alexander Clifford <alexandersinbox@gmail.com>
2020-02-18 22:37:00 -0800

    Filters already saved posts from api fetch search. Implements throttling of requests to not kill HN

--- server/job_provider.py
+++ server/job_provider.py
@@ -1,39 +1,51 @@
 import requests
+import time
 from datetime import date
-from itertools import dropwhile
+from itertools import dropwhile, takewhile
 
 class JobProvider:
     hiring_string = 'ho is hiring?'
     hiring_user = 'whoishiring'
 
-    def __init__(self, jobs_api_url, historical_limit = 2):
+    def __init__(self, jobs_api_url, throttle_group_size = None, throttle_duration = 1):
         self.jobs_api_url = jobs_api_url
-        self.historical_limit = historical_limit
+        self.throttle_group_size = throttle_group_size
+        self.throttle_duration = throttle_duration
 
     # This class is db agnostic, pass in lowest_saved_id if you have anything saved or you'll get the same result each time 
-    def get_next_historical(self, lowest_saved_id = None):
+    def get_next_post(self, historical_limit = 1, lowest_saved_id = None, highest_saved_id = None):
         whois_post_ids = self.get_json(f'/user/{self.hiring_user}/submitted.json')
-        post_ids_iter = iter(whois_post_ids) if lowest_saved_id is None else dropwhile(lambda x: x >= lowest_saved_id)
+        post_ids_iter = iter(whois_post_ids) if lowest_saved_id is None or highest_saved_id is None else (
+            # Overoptimization perhaps? assumption is that there's going to be more than 2x posts on the right tail than we'll iterate through here
+            takewhile(lambda x: x > highest_saved_id) + dropwhile(lambda x: x >= lowest_saved_id)
+        )
         month_posts = None
 
         # TODO: upgrade this to walrus operator once you can bump to python 3.8
         next_id = next(post_ids_iter, None)
         while month_posts is None and next_id is not None:
-            month_posts = self.get_hiring_posts(next_id)
+            month_posts = self.get_hiring_posts(next_id, historical_limit)
             next_id = next(post_ids_iter, None)
 
         return month_posts
 
-    def get_hiring_posts(self, parent_id):
+    def get_hiring_posts(self, parent_id, historical_limit):
         posts = []
         hiring_post = self.get_json(f'/item/{parent_id}.json')
-        if not self.is_valid_post(date.fromtimestamp(hiring_post['time']), hiring_post['title'], self.historical_limit, self.hiring_string):
+        if not self.is_valid_post(date.fromtimestamp(hiring_post['time']), hiring_post['title'], historical_limit, self.hiring_string):
             return None
         else:
             parent_date = hiring_post['time']
-            for child_id in hiring_post['kids'][:5]:
+            for i, child_id in enumerate(hiring_post['kids']):
+                # TODO: update this if you intend for concurrent users
+                if self.throttle_group_size is not None:
+                    if i % self.throttle_group_size == 0:
+                        time.sleep(self.throttle_duration)
+
                 child_post = self.get_json(f'/item/{child_id}.json')
-                posts.append(self.format_post(child_post, parent_date))
+                # TODO: injestion validation if there's too much junk
+                if child_post is not None and child_post.get('parent') == parent_id:
+                    posts.append(self.parse_post(child_post, parent_date))
 
             return posts
 
@@ -58,11 +70,23 @@ class JobProvider:
         )
 
     @staticmethod
-    def format_post(post, parent_date):
+    def parse_post(post, parent_date):
         return {
             'by': post.get('by'),
             'text': post.get('text'),
             'id': post.get('id'),
+            'parent': post.get('parent'),
             'date': parent_date,
             'preferred': None
+        }
+
+    @staticmethod
+    def format_post(post):
+        return {
+            'by': post.get('by'),
+            'text': post.get('text'),
+            'id': post.get('id'),
+            'parent': post.get('parent'),
+            'date': post.get('date'),
+            'preferred': post.get('preferred')
         }
--- server/server.py
+++ server/server.py
@@ -7,25 +7,42 @@ from job_provider import JobProvider
 
 app = Flask(__name__, static_folder='../web-app/build')
 client = MongoClient('localhost', 27017)
-db = client['job-search-database']
-
+# TODO: Config driven
 jobs_host = 'https://hacker-news.firebaseio.com/v0'
-historical_limit = 2
+historical_limit = 1
+throttle_group_size = 20
+throttle_duration = 1
+db_name = 'job-search-database'
+table_name = 'jobs'
+label_key = 'preferred'
 
-job_provider = JobProvider(jobs_host, historical_limit)
+db = client[db_name]
+jobs_table = db[table_name]
+
+job_provider = JobProvider(jobs_host, throttle_group_size, throttle_duration)
 
 @app.route('/api/jobs', methods=['POST'])
 def loadJobs():
     # jobsResponse = requests.get(f'{jobs_host}/v4beta1/jobs:search')
     pass
 
-@app.route('/api/jobs')
+@app.route('/api/jobs/unlabeled')
 def getJobs():
-    new_jobs = job_provider.get_next_historical()
-    if new_jobs is not None:
-        return jsonify(new_jobs)
+    unlabeled_jobs = list(jobs_table.find({ label_key: None }))
+    if len(unlabeled_jobs) != 0:
+        return jsonify([JobProvider.format_post(x) for x in unlabeled_jobs])
     else:
-        abort(404)
+        oldest_saved_post = jobs_table.find_one(sort=[('parent', 1)])
+        newest_saved_post = jobs_table.find_one(sort=[('parent', -1)])
+        oldest_id, newest_id = None, None if oldest_saved_post is None or newest_saved_post is None else (
+            oldest_saved_post['parent'], newest_saved_post['parent']
+        )
+        new_jobs = job_provider.get_next_post(historical_limit, oldest_id, newest_id)
+        if new_jobs is not None:
+            jobs_table.insert_many(new_jobs)
+            return jsonify([JobProvider.format_post(x) for x in new_jobs])
+        else:
+            abort(404)
 
 @app.route('/api/jobs/<id>', methods=['PUT'])
 def labelJob(id):
